4defcd7da280821bdd8e1fe7b11cad34cf765cee
yzwgit <yzwgame@gmail.com>
2020-03-09 21:32:03 +0900

    #5 ヘッダー作成

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="header">
+    <nuxt-link class="header-logo" to="/">
+      <img src="@/assets/img/logo.png" alt="unko" />
+    </nuxt-link>
+    <div class=" header-icon">
+      <img src="@/assets/img/icon.png" class="rounded-full" alt="icon" />
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  @apply flex items-center justify-between flex-wrap bg-teal-500 px-2 py-2;
+}
+
+.header-logo {
+  @apply flex items-center flex-shrink-0 text-white mr-6;
+}
+
+.header-icon {
+  @apply flex items-center px-2 text-teal-200 border-teal-400 rounded-full block;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">rapsoludejo</h1>
-      <h2 class="subtitle">Utilities for playing board games.</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+  <div>
+    <Header />
+    <div class="container">
+      <div>
+        <Logo />
+        <h1 class="title">rapsoludejo</h1>
+        <h2 class="subtitle">Utilities for playing board games.</h2>
+        <div class="links">
+          <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+            >Documentation</a
+          >
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey"
+            >GitHub</a
+          >
+        </div>
       </div>
     </div>
+    <dir></dir>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header.vue'
 
 @Component({
-  components: { Logo },
+  components: { Logo, Header },
 })
 export default class index extends Vue {}
 </script>
