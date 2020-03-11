1eba2f1121b82fcda2dde342ae60681a26a96001
akgm3i <Akigami.Sai@gmail.com>
2020-03-11 17:04:28 +0900

    hero追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    <div class="container"></div>
+  <div class="contents">
+    <div class="hero">
+      <h1 class="message">
+        <span>ボードゲームを</span>
+        <span class="pl-4">もっと楽しく。</span>
+      </h1>
+      <div class="button">
+        <nuxt-link to="/">はじめる</nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Logo from '~/components/Logo.vue'
 
 @Component({
-  components: { Logo },
+  components: {},
 })
 export default class index extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.contents {
+  @apply h-screen py-12;
+}
+
+.hero {
+  background: linear-gradient(#7b341e30, #feebc820), url('~assets/img/hero.jpg');
+  @apply flex flex-col items-center h-full bg-cover bg-center pt-20;
+}
+
+.message {
+  @apply flex flex-col text-center text-4xl text-gray-100;
+}
+
+.button {
+  @apply mt-8 px-4 py-1 bg-blue-500 rounded-lg text-xl text-gray-100;
+}
+</style>
